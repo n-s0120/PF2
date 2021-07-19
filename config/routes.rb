09bd2c34072de368ec9693fbbff1b0726d95be96
ns-0120 <satoshicuni@gmail.com>
2021-07-19 10:22:23 +0000

    フローチャートの雛形作成、タブ機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
   devise_for :users
   root to: 'homes#top'
   get '/search' => 'searches#search'
@@ -6,4 +9,5 @@ Rails.application.routes.draw do
     resources :comments, only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
   end
+  resources :flowcharts, only: [:new, :create, :index, :show, :destroy]
 end
